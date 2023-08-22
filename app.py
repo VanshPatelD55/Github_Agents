b6507bb7e831b91f2191fd39e26e42dbc02f28ba
VanshPatelD55 <138886011+VanshPatelD55@users.noreply.github.com>
2023-08-22 11:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,40 +32,45 @@ if github_repo and github_branch and github_base_branch:
 
 
 prompt = st.text_area("Prompt")
-final_prompt = f""" 
-       You are a skilled software developer with an in-depth understanding of various programming languages and proficient in performing GitHub operations. 
-       When generating code, please only include code snippets from the given file. 
-       Refrain from adding any additional content to the response. 
-       Ensure that the code provided is correctly formatted and, if necessary, you can complete code snippets that might have errors to ensure they compile successfully.
-       
-       Strictly do not give any other text only and only give code as an output
-       follow the above given instructions while performing prompt given below by the user
-       prompt:{prompt}
-"""
+agent_has_run = False
 
-# Initialize components
-llm = OpenAI(temperature=0)
-github = GitHubAPIWrapper()
-toolkit = GitHubToolkit.from_github_api_wrapper(github)
-agent = initialize_agent(
-    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-)
-response = ""
+if github_repo and github_branch and github_base_branch:
+    os.environ["GITHUB_REPOSITORY"] = github_repo
+    os.environ["GITHUB_BRANCH"] = github_branch
+    os.environ["GITHUB_BASE_BRANCH"] = github_base_branch
 
-# Run the agent when the user clicks the "Run" button
 if st.button("Run Agent"):
     if github_repo and github_branch and github_base_branch and prompt:
-        os.environ["GITHUB_REPOSITORY"] = github_repo
-        os.environ["GITHUB_BRANCH"] = github_branch
-        os.environ["GITHUB_BASE_BRANCH"] = github_base_branch
-
-        # Run the agent
-        response = agent.run(final_prompt)
-        st.write("Agent Response:")
-        st.write(response)
-    else:
-        st.warning("Please fill in all the input fields.")
-
-with CodeBox() as codebox:
-    result = codebox.run(response)
-    st.write(result)
+        # Initialize components
+        llm = OpenAI(temperature=0)
+        github = GitHubAPIWrapper()
+        toolkit = GitHubToolkit.from_github_api_wrapper(github)
+        agent = initialize_agent(
+            toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+        )
+        
+        if not agent_has_run:
+            agent_has_run = True
+            final_prompt = f""" 
+               You are a skilled software developer with an in-depth understanding of various programming languages and proficient in performing GitHub operations. 
+               When generating code, please only include code snippets from the given file. 
+               Refrain from adding any additional content to the response. 
+               Ensure that the code provided is correctly formatted and, if necessary, you can complete code snippets that might have errors to ensure they compile successfully.
+               
+               Strictly do not give any other text only and only give code as an output
+               follow the above given instructions while performing prompt given below by the user
+               prompt:{prompt}
+            """
+            
+            # Run the agent
+            response = agent.run(final_prompt)
+            st.write("Agent Response:")
+            st.write(response)
+            
+            # Display code result
+            with CodeBox() as codebox:
+                result = codebox.run(response)
+                st.write(result)
+                
+        else:
+            st.warning("Agent has already been run. Please reset the input fields to run again.")
