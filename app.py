d5dd13cd99af480292e1b6e47a491ba517b36fb6
VanshPatelD55 <138886011+VanshPatelD55@users.noreply.github.com>
2023-08-22 09:12:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,6 @@ if st.button("Run Agent"):
     else:
         st.warning("Please fill in all the input fields.")
 
-with CodeBox() as codebox:
-    result = codebox.run(response)
-    st.write("<h2>" + response + "</h2>", unsafe_allow_html=True)
+# with CodeBox() as codebox:
+#     result = codebox.run(response)
+#     st.write("<h2>" + response + "</h2>", unsafe_allow_html=True)
